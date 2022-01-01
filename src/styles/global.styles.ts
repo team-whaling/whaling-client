687977e5915233a6c046be387c314240c0ee83b7
Kiuk <ugiya0628@icloud.com>
2022-01-01 16:25:30 +0900

    [WHAL-19] feat: add global style (#9)
    * [WHAL-19] feat: add Text Component for globau usage
    
    * [WHAL-19] refactor: change name of the Text Component
    
    * [WHAL-19] feat: add whale svg  icon in static/icons
    
    * [WHAL-19] feat: add global style

--- src/styles/global.styles.ts
+++ src/styles/global.styles.ts
@@ -1,1 +1,9 @@
-export {};
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+* {
+    font-family: 'Noto Sans KR', sans-serif;
+}
+`;
+
+export default GlobalStyle;
