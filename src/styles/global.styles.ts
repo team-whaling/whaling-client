787fefe66a68df7011a6f6fc7938b2a8370ac785
kiuk <ugiya0628@icloud.com>
2022-01-01 11:12:53 +0100

    [WHAL-19] feat: set html body size to  Phone Size

--- src/styles/global.styles.ts
+++ src/styles/global.styles.ts
@@ -4,6 +4,25 @@ const GlobalStyle = createGlobalStyle`
 * {
     font-family: 'Noto Sans KR', sans-serif;
 }
+
+html {
+    width: 100vw;
+    height: 100vh;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    
+    padding: 0;
+    margin: 0;
+}
+
+body {
+    width: 375px;
+    height: 812px;
+
+    border: 1px solid black
+}
 `;
 
 export default GlobalStyle;
