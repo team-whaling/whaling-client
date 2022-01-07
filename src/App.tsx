50a0a11ae4b2b801fe1814144b45f01cdf71da4f
kiuk <ugiya0628@icloud.com>
2022-01-07 04:37:05 +0100

    [WHAL-19] feat: add Global Icon Component

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,20 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import GlobalStyle from './styles/global.styles';
+import CreateVote from './pages/createVote';
+import Login from './pages/login';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <>
       <GlobalStyle />
-      <div></div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/create" element={<CreateVote />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
