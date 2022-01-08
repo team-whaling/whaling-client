252ba851d7b3e784e595db40720877d8aba7ea84
Taeeun Kim <xodms0309@naver.com>
2022-01-08 17:16:18 +0900

    style: modify color

--- src/styles/color.ts
+++ src/styles/color.ts
@@ -1,19 +1,19 @@
 const color = {
   darkness: {
-    1: '#F5F5F5',
-    2: '#ECECEC',
-    3: '#C6C6C6',
-    4: '#A0A0A0',
-    5: '#7A7A7A',
-    6: '#414141',
+    0: '#FFFFFF', //white
+    1: '#FBFBFB',
+    2: '#F2F4F6',
+    3: '#E8E9EB',
+    4: '#C6C8CB',
+    5: '#979CA3',
+    6: '#676C74',
+    7: '#2B2D31',
   },
   blue: {
     1: '#DADCFA',
     2: '#A2A8F2',
     3: '#7A82ED',
     4: '#4652E6',
-    5: '#A4FAFF',
-    6: '#DAFDFF',
   },
   red: {
     1: '#FFDAD5',
