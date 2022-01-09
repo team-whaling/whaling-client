a22da3d06607fece8652fe1ecd8f491e522cc074
kiuk <ugiya0628@icloud.com>
2022-01-09 09:34:05 +0100

    [WHAL-19] feat: add global wrapper component

---
+++ src/components/Layout.tsx
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const Column = styled.div`
+  all: unset;
+
+  display: flex;
+  flex-direction: column;
+`;
+
+export const Row = styled.div`
+  all: unset;
+
+  display: flex;
+  flex-direction: row;
+`;
