9cd20eca2291396b736d4cb703fbd726142035c0
kiuk <ugiya0628@icloud.com>
2022-01-16 19:25:22 +0100

    [WHAL-55] feat: add my-page route

--- src/pages/myPage/index.tsx
+++ src/pages/myPage/index.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import Icon, { IconType, TIcon } from '../../components/Icon';
+import { Column } from '../../components/Layout';
 
 const index = () => {
-  return <div></div>;
+  return (
+    <Column>
+      <Icon iconType={IconType.HeaderBack as TIcon} />
+    </Column>
+  );
 };
 
 export default index;
