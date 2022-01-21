fd49c5ad60951a31e1006df2cf0a8d5881c86c11
Taeeun <xodms0309@naver.com>
2022-01-21 16:57:23 +0900

    feat: create vote card styles according to thier types

--- src/components/Card/VoteCard.tsx
+++ src/components/Card/VoteCard.tsx
@@ -5,7 +5,11 @@ import Icon from '../Icon';
 import Text from '../Text';
 import direction from '../../static/icons/direction.svg';
 import { Row, RowBetween, RowCenter } from '../Layout';
+import BarGraph from '../graph/BarGraph';
 const VoteCard = () => {
+  // TODO: type은 props로 받을 것
+  //type은 ongoing, completed, tracked
+  let type = 'tracked';
   return (
     <Container>
       <RowBetween>
@@ -16,21 +20,39 @@ const VoteCard = () => {
       </RowBetween>
       <Text type="Body" content="$스테이터스네트워크토큰이" />
       <Text type="Body" content="1개월 후에 10%이상 오를까요?" />
-      <RowBetween style={{ marginTop: 14 }}>
-        <Text
-          type="Body2"
-          content="적중시 고래밥 20개 증정!"
-          style={{ color: `${color.darkness[5]}` }}
-        />
-        <RowCenter>
-          <Text
-            type="Body"
-            content="참여하기"
-            style={{ margin: 0, color: `${color.blue[4]}` }}
-          />
-          <DirectionIcon src={direction} />
-        </RowCenter>
-      </RowBetween>
+      {type === 'tracked' ? (
+        <div style={{ marginTop: 9 }}>
+          <BarGraph kind="card" />
+        </div>
+      ) : (
+        <RowBetween style={{ marginTop: 14 }}>
+          {type === 'ongoing' ? (
+            <Text
+              type="Body2"
+              content="적중시 고래밥 20개 증정!"
+              style={{ color: `${color.darkness[5]}` }}
+            />
+          ) : (
+            <Text
+              type="Body2"
+              content="결과가 변화했는지 확인해볼까요? "
+              style={{ color: `${color.darkness[5]}` }}
+            />
+          )}
+          <RowCenter>
+            {type === 'ongoing' ? (
+              <Text
+                type="Body"
+                content="참여하기"
+                style={{ color: `${color.blue[4]}` }}
+              />
+            ) : (
+              <Text type="Body" content="참여 완료" />
+            )}
+            <DirectionIcon src={direction} />
+          </RowCenter>
+        </RowBetween>
+      )}
     </Container>
   );
 };
