1dd76d2fa9fd9e6f2647b14808a21945601875e2
Taeeun <xodms0309@naver.com>
2022-01-22 14:58:10 +0900

    feat: create initial card

---
+++ src/components/card/InitialCard.tsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import styled from 'styled-components';
+import color from '../../styles/color';
+import Text from '../Text';
+import Image from '../Image';
+import { Column, Row, RowCenter } from '../Layout';
+const InitialCard = () => {
+  return (
+    <>
+      <Container>
+        <Column style={{ padding: '18px 24px' }}>
+          <Text
+            type="Body2"
+            content="적중률 100% 도전기!"
+            style={{ marginBottom: 13 }}
+          />
+          <Text type="Headline" content="아직 진행하신 " />
+          <Row>
+            <Text type="Headline" content="투표가" />
+            <Text
+              type="Headline"
+              content=" 없어요!"
+              style={{ whiteSpace: 'pre-wrap', color: `${color.blue[4]}` }}
+            />
+          </Row>
+          <Text type="Headline" content="투표하러 가볼까요?" />
+        </Column>
+        <Image imgType="Note" />
+      </Container>
+      <VoteButton>
+        <Text
+          type="Headline2"
+          content="투표하러가기"
+          style={{
+            color: '#ffffff',
+          }}
+        />
+      </VoteButton>
+    </>
+  );
+};
+const Container = styled(Row)`
+  width: 343px;
+  height: 149px;
+
+  margin-top: 16px;
+
+  background: #f2f6fa;
+  border-radius: 10px 10px 0 0;
+`;
+
+const VoteButton = styled(RowCenter)`
+  width: 343px;
+  height: 55px;
+
+  background: ${color.blue[4]};
+  border-radius: 0px 0px 10px 10px;
+`;
+
+export default InitialCard;
